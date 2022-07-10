a9f5068bb59ba693476a6d8c8ab33c4f8e11122f
byuneunhye <gracebyun06@gmail.com>
2022-07-10 22:17:21 +0900

    20220709 Codeup1228.C

---
+++ 1228.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    double a, b;
+    scanf("%lf %lf",&a,&b);
+    double w,x;
+    w=(float)(a-100)*0.9;
+    x=(float)(b-w)*100/w;
+    if(x<=10)
+       printf("정상");
+    else if(x>10&&x<=20)
+        printf("과체중");
+    else if(x>20)
+        printf("비만");
+  // printf(x<=10?"정상":?"과체중":"비만");
+
+}
+
+
+
